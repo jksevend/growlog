cb53a4f62a83dbda30ef5b74f39cc71401275fb4
Julian Otto <julxott@outlook.de>
2024-07-07 15:55:48 +0200

    feat: Prepare for v1.2.0

--- lib/settings/view.dart
+++ lib/settings/view.dart
@@ -140,7 +140,7 @@ class _SettingsViewState extends State<SettingsView> {
                           applicationIcon:
                               Image.asset('assets/img/app_icon.png', width: 50, height: 50),
                           applicationName: 'GrowLog - Cannabis diary',
-                          applicationVersion: '1.1.0',
+                          applicationVersion: '1.2.0',
                           applicationLegalese:
                               'MIT LICENSE 2024 GrowLog\ngithub.com/jksevend/growlog',
                         );
